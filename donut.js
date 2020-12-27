eea38875c2f23fd50b077f01ea6f4116563200d9
Inanc <inancakduvann@gmail.com>
2020-12-27 15:32:40 +0300

    background config added

--- donut.js
+++ donut.js
@@ -3,6 +3,9 @@ function donut(id, config) {
     var rest_val = (100-config.value);
     var val = slice_val + " " + rest_val;
 
+    var background =  config.background ? config.background : "#ebebeb";
+    var color = config.color ? config.color : "#ff0";
+
     var template = ' \
     <div class="svg-item"> \
     <style> \
@@ -19,8 +22,8 @@ function donut(id, config) {
     } \
     </style> \
         <svg width="100%" height="100%" viewBox="0 0 40 40" class="donut" style="margin:0 auto;"> \
-          <circle class="donut-flat" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke="#ebebeb" stroke-width="3.5"></circle> \
-          <circle class="donut-slice" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5" stroke="'+ config.color +'" stroke-dasharray="'+ val +'" stroke-dashoffset="25"></circle> \
+          <circle class="donut-flat" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke="'+ background +'" stroke-width="3.5"></circle> \
+          <circle class="donut-slice" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5" stroke="'+ color +'" stroke-dasharray="'+ val +'" stroke-dashoffset="25"></circle> \
         </svg> \
       </div> \
     '
@@ -30,5 +33,6 @@ function donut(id, config) {
 
 donut("chart", {
     value: 80,
-    color: "lightgreen"
+    color: "lightgreen",
+    background: "#eee"
 });
