13abe658716fc0e1cc1875d97d7a03d0b4c3e71a
Inanc <inancakduvann@gmail.com>
2020-12-27 15:25:33 +0300

    value and color dynamically drawn

--- donut.js
+++ donut.js
@@ -1,4 +1,8 @@
-function donut(id) {
+function donut(id, config) {
+    var slice_val = config.value;
+    var rest_val = (100-config.value);
+    var val = slice_val + " " + rest_val;
+
     var template = ' \
     <div class="svg-item"> \
     <style> \
@@ -10,13 +14,13 @@ function donut(id) {
             stroke-dasharray: 0, 100; \
         } \
         100% { \
-            stroke-dasharray: 90, 10; \
+            stroke-dasharray: '+ slice_val +', '+ rest_val +'; \
         } \
     } \
     </style> \
         <svg width="100%" height="100%" viewBox="0 0 40 40" class="donut" style="margin:0 auto;"> \
           <circle class="donut-flat" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke="#ebebeb" stroke-width="3.5"></circle> \
-          <circle class="donut-slice" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5" stroke="#ff6200" stroke-dasharray="90 10" stroke-dashoffset="25"></circle> \
+          <circle class="donut-slice" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5" stroke="'+ config.color +'" stroke-dasharray="'+ val +'" stroke-dashoffset="25"></circle> \
         </svg> \
       </div> \
     '
@@ -24,4 +28,7 @@ function donut(id) {
     document.getElementById(id).innerHTML = template;
 }
 
-donut("chart");
+donut("chart", {
+    value: 80,
+    color: "lightgreen"
+});
